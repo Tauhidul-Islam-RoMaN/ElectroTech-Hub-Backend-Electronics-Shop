e0d94b606d99135639bf0373f21622661922ca6f
674RoMaN <tauhidroman@gmail.com>
2023-10-17 22:17:16 +0600

    Basic setup done

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.listen(port, ()=> {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h1i8cb8.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
